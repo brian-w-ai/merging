33a22e3d14dfa2de04a5b85e6e3b872b7c4e0b7d
Brian Ai <brianai@gmail.com>
2022-03-31 11:27:11 -0700

    some files

---
+++ AutoLayout/AutoLayout/PostCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostCell.swift
+//  AutoLayout
+//
+//  Created by Brian Ai on 2/17/22.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Brian Ai on 2/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
